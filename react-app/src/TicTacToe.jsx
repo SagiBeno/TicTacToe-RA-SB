c76337a4dfe109251e74e282a98fd42d9c56525e
hajos8 <rabb.andras@gmail.com>
2025-10-17 13:03:54 +0200

    Fixed calculateWinner

--- react-app/src/TicTacToe.jsx
+++ react-app/src/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,Fragment } from 'react';
 // npm install express cors react-toastify
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -32,21 +32,47 @@ function TicTacToe() {
   };
   // TODO - implement backend GET: query previous match results
 
-  const calculateWinner = function(board) {/* TODO - implement */}
+  const calculateWinner = function(board) {
+    let win = false;
+
+    for (let i = 0; i < 3; i++) {
+      //row
+      if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+        win = true;
+      } //column
+      else if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        win = true;
+      }
+    }
+    //diagonal
+    if (board[0][0] && board[1][1] && board[2][2] //check not null
+      && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      win = true;
+    }
+    else if (board[0][2] && board[1][1] && board[2][0] //check not null
+      && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      win = true;
+    }
+
+    return win;
+  }
 
   const handleClick = (idx) => {
-    console.log('handleClick idx: ', idx)
-    //TODO - if (board[idx] || winner) return;
     const newBoard = board.slice()
     newBoard[idx[0]][idx[1]] = nextPlayer;
+
+    console.log('New board: ', newBoard);
+
     setBoard(newBoard);
-    setNextPlayer('O'); // TODO
+    setNextPlayer(nextPlayer === 'X' ? 'O' : 'X');
+
     const win = calculateWinner(newBoard);
     if (win) {
-      setWinner(win);
-      toast(`${win} wins!`);
-      postResult(`${win} wins`);
-    } else if (!newBoard.includes(null)) {
+      setWinner(nextPlayer);
+      toast(`${nextPlayer} wins!`);
+      postResult(`${nextPlayer} wins`);
+    } 
+    else if (!newBoard.flat().includes(null)) {
       toast('Draw!');
       postResult('Draw');
     }
@@ -78,16 +104,16 @@ return (
           <div className="board-row" key={row}>
             {[0,1,2].map(col => {
               const idx = [row, col];
-              return <>
-                {board[row][col] != null ? <button className={`square ${board[row][col] == "X" ? "playerX" : "playerO"}`}>{board[row][col]}</button> 
-                    :
-                    <button 
-                      key={col}
-                      className="square" 
-                      onClick={() => handleClick(idx)}
-                    />
-                  }
-                </>;
+              return <Fragment key={row + col / 10}>
+                  {board[row][col] != null ? <button className={`square, ${board[row][col] == "X" ? "playerX" : "playerO"}`}>{board[row][col]}</button> 
+                      :
+                      <button 
+                        key={row + col / 10}
+                        className="square" 
+                        onClick={() => handleClick(idx)}
+                      />
+                    }
+                </Fragment>;
             })}
           </div>
         ))
