80051f416d0e21e3e0a70adabd7775f733b79227
hajos8 <rabb.andras@gmail.com>
2025-10-17 11:03:13 +0200

    Basic get and post method

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -5,6 +5,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const results = [];
+
+app.get('/api/results', (req, res) => {
+    res.status(200).json(results);
+});
+
+app.post('/api/result', (req, res) => {
+    const { result } = req.body;
+    results.push(result);
+    res.status(201).json(results);
+});
+
 const port = 3333;
 
 app.listen(port, () => {
